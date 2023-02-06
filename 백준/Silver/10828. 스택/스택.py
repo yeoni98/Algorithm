2a6cf440d66dbc9d4077424f398a2eab686e95e5
yeoni98 <88011254+yeoni98@users.noreply.github.com>
2023-02-06 12:43:22 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,29 @@
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+
+for i in range(n):
+    a = sys.stdin.readline().split()
+
+    if a[0] == 'push':
+        stack.append(a[1])
+    elif a[0] == 'pop':
+        if stack:
+            print(stack[-1])
+            stack.pop()
+
+        else:
+            print('-1')
+    elif a[0] == 'size':
+        print(len(stack))
+    elif a[0] == 'empty':
+        if len(stack) == 0:
+            print('1')
+        else:
+            print('0')
+    elif a[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print('-1')
