369053d2dda8e0c5cea9c45d6559206056d66ee5
yeoni98 <88011254+yeoni98@users.noreply.github.com>
2023-02-17 12:44:50 +0900

    Update 회전 초밥.py

--- 백준/Gold/15961. 회전 초밥/회전 초밥.py
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.py
@@ -16,6 +16,10 @@ eat[c] += 1
 
 def maximum(sushi):
     global cnt, right, left
+#global 선언을 꼭 해줘야됨 변수 cnt, right,left가 maximum함수 안에 존재하지 않으니 a를 전역네임스페이스에서 함수안으로 땡겨와 사용할 수 있게 한 것이다.
+#따라서 global선언 없이 함수 내에서 전역변수를 사용할 경우, print(cnt) , if cnt == 1: 과 같이 조회하는 기능만 가능하다.
+#cnt값을 변경하는 것과 같이 "할당"(예를 들어 cnt = max(cnt, len(eat))을 해주려면 global선언이 필수적이다.
+#참고:https://velog.io/@hongsikcho/%ED%8C%8C%EC%9D%B4%EC%8D%AC-referenced-before-assignment%EC%97%90%EB%9F%AC-%EC%9D%B4%EC%9C%A0%EC%99%80-%ED%95%B4%EA%B2%B0
 
     for right in range(len(sushi)):
         eat[sushi[right]] +=1
