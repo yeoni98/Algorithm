aec1d75063396ff5f0ca6c930a3462ff6b032f10
yeoni98 <88011254+yeoni98@users.noreply.github.com>
2023-02-23 18:42:45 +0900

    [Silver V] Title: 덩치, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,14 @@
+import sys
+student = int(sys.stdin.readline())
+student_list = []
+
+for _ in range(student):
+    weight, height = map(int, sys.stdin.readline().split())
+    student_list.append((weight, height))
+
+for i in student_list:
+    rank = 1
+    for j in student_list:
+        if i[0] < j[0] and i[1] < j[1]:
+                rank += 1
+    print(rank, end = " ")
