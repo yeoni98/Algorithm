0476d72a32346c5269cc8f3685e520788f70ceed
yeoni98 <88011254+yeoni98@users.noreply.github.com>
2023-02-27 18:44:04 +0900

    [Gold IV] Title: 연구소, Time: 2696 ms, Memory: 129560 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,48 +1,57 @@
-import sys; input = sys.stdin.readline
+from collections import deque
 import copy
+import sys
+input = sys.stdin.readline
 
-# 벽 건설 함수
-def wall_construct(wall_cnt):
-    # 벽 3개 설치 완료 시, 바이러스 전파
-    if wall_cnt == 3:
-        virus_spread()
+d = [[-1,0],[1,0],[0,-1],[0,1]]
+
+def bfs():
+    queue = deque()
+    #queue에 2의 위치 전부 append
+    test_map = copy.deepcopy(lab_map)
+    for i in range(n):
+        for k in range(m):
+            if test_map[i][k] == 2:
+                queue.append((i,k))
+
+    while queue:
+        r,c = queue.popleft()
+
+        for i in range(4):
+            dr = r+d[i][0]
+            dc = c+d[i][1]
+
+            if (0<=dr<n) and (0<=dc<m):
+                if test_map[dr][dc] == 0:
+                    test_map[dr][dc] =2
+                    queue.append((dr,dc))
+
+    global result
+    count = 0
+    for i in range(n):
+        for k in range(m):
+            if test_map[i][k] == 0:
+                count +=1
+
+    result = max(result, count)
+
+
+def make_wall(count):
+    if count == 3:
+        bfs()
         return
-    # 벽을 설치할 수 있는 모든 경우의 수 확인
-    for n in range(N):
-        for m in range(M):
-            if board[n][m] == 0:
-                board[n][m] = 1 # 벽 설치
-                wall_construct(wall_cnt + 1)
-                board[n][m] = 0 # 새로운 조합 만들기 위해 초기화
+    for i in range(n):
+        for k in range(m):
+            if lab_map[i][k] == 0:
+                lab_map[i][k] = 1
+                make_wall(count+1)
+                lab_map[i][k] = 0
 
-# 바이러스 전파 함수
-def virus_spread():
-    global answer
-    # 기존 맵 정보 깊은 복사(Deep Copy)
-    board_wall = copy.deepcopy(board)
-    # 바이러스 위치
-    virus = [(n, m) for n in range(N) for m in range(M) if board_wall[n][m] == 2]
 
-    # 바이러스마다 전파 끝날 때까지 반복
-    while virus:
-        x, y = virus.pop()
-        for dx, dy in direction:
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < N and 0 <= ny < M and board_wall[nx][ny] == 0:
-                board_wall[nx][ny] = 2
-                virus.append((nx, ny)) # 바이러스 전파
+n, m = map(int,input().split())
+lab_map = [list(map(int,input().split())) for _ in range(n)]
 
-    # 안전지대 개수 카운팅
-    safezone_cnt = 0
-    for row in board_wall:
-        safezone_cnt += row.count(0)
-    answer = max(answer, safezone_cnt)
+result = 0
+make_wall(0)
 
-if __name__ == "__main__":
-    N, M = map(int, input().split())
-    board = [list(map(int, input().split())) for _ in range(N)]
-    direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    answer = 0
-    wall_construct(0)
-    print(answer)
+print(result)
