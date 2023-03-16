5cdf85b07f2187e967229005eeffc840be52292f
yeoni98 <88011254+yeoni98@users.noreply.github.com>
2023-03-16 14:15:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int rst = 0;
+		
+		while(n!=0) {
+			if (n%5==0) {
+				rst += n/5;
+				n%=5;
+				System.out.println(rst);
+				break;
+			}
+			n-=3;
+			rst +=1;
+			if (n==0) {System.out.println(rst); break;}
+			if(n<0) {System.out.println(-1); break;}
+			
+		}
+		
+
+	}
+
+}
