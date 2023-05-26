3effb129cdcf74e43a4bb493c44007c0c41a7af5
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-05-26 11:30:26 +0900

    [level 2] Title: 올바른 괄호, Time: 7.27 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,23 +1,21 @@
+from collections import deque
 def solution(s):
-
-    q = deque()
+    q =  deque()
+    
     q.append(s[0])
-    for i in range(1,len(s)):
-        if s[i] == ')':
-            if len(q) != 0 :
-                q.pop()
-            else: 
-                answer = False
-                    
-        if s[i] == '(':
-            q.append(s[i])
-    if len(q)>0:
+    
+    for i in s[1:]:
+        if i == "(":
+            q.append(i)
+        elif i == ")":
+            if not q:
+                return False
+            q.pop()
+
+    if q:
         answer = False
     else:
         answer = True
-    
 
     return answer
-
-
-from collections import deque
+    
