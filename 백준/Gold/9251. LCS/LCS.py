a9ee45ef3debb8974d88b1c748f88bb6cc275d47
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-06-10 10:14:12 +0900

    [Gold V] Title: LCS, Time: 536 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,15 @@
+import sys
+
+
+a = ' '+sys.stdin.readline().rstrip()
+b = ' '+ sys.stdin.readline().rstrip()
+
+dp = [[0]*(len(b)) for _ in range(len(a))]
+
+for i in range(1,len(a)):
+    for j in range(1,len(b)):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+print(dp[-1][-1])
