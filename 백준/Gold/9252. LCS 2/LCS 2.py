ac22c73796f5ba90d1f66bfe5d013cd76e36769f
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-06-14 10:48:22 +0900

    [Gold IV] Title: LCS 2, Time: 628 ms, Memory: 481772 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,21 @@
+import sys
+
+a = ' '+sys.stdin.readline().rstrip()
+b = ' '+sys.stdin.readline().rstrip()
+
+dp = [['']*len(b) for _ in range(len(a))]
+
+
+for i in range(1,len(a)):
+    for j in range(1,len(b)):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i-1][j-1]+b[j]
+
+        else:
+            if len(dp[i-1][j]) >= len(dp[i][j-1]):
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i][j-1]
+
+print(len(dp[-1][-1]))
+print(dp[-1][-1])
