3ce21a2b77de4a6107b39505869fd89406092392
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-06-18 22:56:09 +0900

    [Gold V] Title: LCS, Time: 532 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,15 +1,18 @@
 import sys
 
+a = list(sys.stdin.readline().rstrip())
+b = list(sys.stdin.readline().rstrip())
 
-a = ' '+sys.stdin.readline().rstrip()
-b = ' '+ sys.stdin.readline().rstrip()
+dp = [[0]*(len(b)+1) for _ in range(len(a)+1)]
 
-dp = [[0]*(len(b)) for _ in range(len(a))]
+for i in range(1,len(a)+1):
+    for j in range(1,len(b)+1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] +1
+            
+        else:                # 왼쪽     ,     위
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+            
 
-for i in range(1,len(a)):
-    for j in range(1,len(b)):
-        if a[i] == b[j]:
-            dp[i][j] = dp[i-1][j-1]+1
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
 print(dp[-1][-1])
