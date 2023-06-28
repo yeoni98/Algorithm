e52e2d611891a9ca5043c83d46f86bdc5c792b44
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-06-28 12:29:37 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 144 ms, Memory: 36144 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,24 +1,25 @@
 import sys
 from collections import deque
 
-n, k = map(int, sys.stdin.readline().split())
+n, k = map(int,sys.stdin.readline().split())
+graph = [0]*100001
+visited = [False]*100001
+q = deque()
+q.append(n)
+while q:
+    x = q.popleft()
+    if x==k:
+        print(graph[x])
+        break
+    for nx in (x-1,x+1,x*2):
+        if 0<=nx<=100000 and visited[nx] == False:
+            if nx == x*2:
+                graph[nx] = graph[x]
+                q.appendleft(nx)
+            else:
+                graph[nx] = graph[x] + 1
+                q.append(nx)
+            visited[nx] = True
 
-def bfs():
-    graph = [0] * 100001
-    graph[n] = 1
-    que = deque([n])
 
-    while que:
-        target = que.popleft()
-        if target == k:
-            return graph[target]
-        for i in (target +1, target -1, target*2):
-            if 0<= i <=100000 and graph[i] == 0:
-                if i == target*2:
-                    graph[i] = graph[target]
-                    que.appendleft(i)
-                else:
-                    graph[i] = graph[target] + 1
-                    que.append(i)
 
-print(bfs()-1)
