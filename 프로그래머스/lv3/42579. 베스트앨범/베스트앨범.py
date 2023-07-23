14d07017ac2d9036f80d12facf5772f13929307b
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-07-23 23:29:57 +0900

    [level 3] Title: 베스트앨범, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,31 +1,33 @@
 def solution(genres, plays):
     answer = []
     
-    dict1 = {}
-    dict2 = {}
-    for i, (k, v) in enumerate(zip(genres, plays)):
-        if k not in dict1:
-            dict1[k] = [(i,v)]
+    dict1 = {} #장르의 인덱스 값
+    dict2 = {} # 각 장르의 총 재생횟수
+    
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        # 0, classic, 500
+        # 1, pop, 600
+        if g not in dict1:
+            dict1[g] = [(i,p)] # dict1[classic] = [(0,500)]
         else:
-            dict1[k].append((i,v))
-        if k not in dict2:
-            dict2[k] = v
-        else:
-            dict2[k] += v
+            dict1[g].append((i,p))
             
-    for k, v in sorted(dict2.items(), key = lambda x:x[1], reverse = True):
+        if g not in dict2:
+            dict2[g] = p
+        else:
+            dict2[g] += p
+    
+    #재생횟수를 기준으로 내림차순으로 정렬
+    for k, v in sorted(dict2.items(), key = lambda x:x[1], reverse = True): 
+        # 해당 장르(k)의 재생횟수를 기준으로 내림차순 정렬(2개만))
         for i, value in sorted(dict1[k], key = lambda x:x[1], reverse = True)[:2]:
+        # dict1['pop'] = [(4, 2500),(1, 600)]
             answer.append(i)
     
     
     return answer
-# 1. dict1 에 k에 따른 (i, v) 저장 len(dict1[k]) 로 나열~
-# 2. dict2[i] = v. i에 대한 v 저장 => 내부에서 v를 내림차순으로 -> 2개만
+# 1. dict1 에 g에 따른 (i, p) 저장 len(dict1[k]) 로 정렬~
+# 2. dict2[g] = p. g에 대한 p 저장 => 내부에서 p를 내림차순으로 -> 2개까지만
 # 3. 
-# 4. cnt 2
-# dict 로 key 별로 +1 하기, 
-# dict.value.sort(reverse = True)
-# 고유 번호 순서 => Dict 로..?
-# 두개씩이니까 확인해야..
-                  
+
 
