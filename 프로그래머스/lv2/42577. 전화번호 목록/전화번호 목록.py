c75bf10c32249ec7a2c4623494fa8c90b514446c
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-07-23 22:57:06 +0900

    [level 2] Title: 전화번호 목록, Time: 98.16 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,5 +1,6 @@
 def solution(phone_book):
     phone_book.sort()
+    #["119", "97674223", "1195524421"] -> "119", "1195~", "976~"
     
     for i in range(len(phone_book)-1):
         if phone_book[i+1][:len(phone_book[i])] == phone_book[i]:
@@ -7,5 +8,3 @@ def solution(phone_book):
             break
     return True
 
-# 길이가 가장 짧은  min - len(min) 으로 다른 것[:len(min)]이 같은가
-# 만약 같은 자리 수의 숫자일 땐 ..? 어떻게 판별..?
