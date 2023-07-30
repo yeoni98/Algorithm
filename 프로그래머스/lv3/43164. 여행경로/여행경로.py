31e3d8d7a48fc00d609090b2aff9d0730ba0982a
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-07-30 22:26:05 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,28 +1,45 @@
-from collections import deque
+# from collections import deque
+
+# def solution(tickets):
+#     answer = []
+#     //start = "ICN"
+#     flight = []
+    
+#     for from, to in tickets:
+#         if from in flight:
+#             flight[from].append(to)
+#         else:
+#             flight[from] = to
+            
+#     def dfs(from):
+#         while flight[from]:
+            
+from collections import defaultdict
 
 def solution(tickets):
-    answer = []
-    path = {}
-    
-    for (start, end) in tickets:
-        if start in path:
-            path[start].append(end)
-        else:
-            path[start] = [end]
-            
-    for p in path.keys():
-        path[p].sort(reverse = True)
-    
-    st = ["ICN"]
-    
-    while st:
-        top = st[-1]
-        if (top not in path) or (not path[top]):
-            answer.append(st.pop())
-            
-        else:
-            st.append(path[top].pop())
-    
-    
-    
-    return answer[::-1]
+    flights = defaultdict(list)
+
+    # 모든 티켓을 각 출발점에 따라 정렬하여 저장합니다.
+    for start, end in sorted(tickets):
+        flights[start].append(end)
+
+    routes = []
+
+    def dfs(start, path):
+        path.append(start)
+
+        # 모든 티켓을 사용했다면, 현재 경로를 가능한 경로에 추가합니다.
+        if len(path) == len(tickets) + 1:
+            routes.append(path.copy())
+            return
+
+        for i in range(len(flights[start])):
+            next = flights[start].pop(i)
+            dfs(next, path)
+            path.pop()
+            flights[start].insert(i, next) 
+
+    dfs('ICN', [])
+
+    return min(routes) 
+        
