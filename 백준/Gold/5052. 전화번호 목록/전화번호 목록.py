af65061f63e2431e402665050ce76e34b09f6e92
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-08-04 16:40:19 +0900

    [Gold IV] Title: 전화번호 목록, Time: 168 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,26 +1,17 @@
+
 import sys
 
 t = int(sys.stdin.readline())
+
 for i in range(t):
     n = int(sys.stdin.readline())
-    num = []
-    cnt = 0
-    if n == 1:
-        print('YES')
-        continue
-
-
-    else:
-        breaker = False
-        for j in range(n):
-            num.append(sys.stdin.readline().rstrip())
-        num.sort() # 전화번호는 문자열로 인식되어 Ex) 정렬 시 '1234',  '145', '12345' -> '1234', '12345', '145'
-        for j in range(n-1):
-            # print(num[j], num[j + 1][:len(num[j])])
-            # 문자열로 정렬되었으므로 바로 옆만 비교
-            if num[j] == num[j+1][:len(num[j])]:
-                print("NO")
-                breaker = True
-                break
-        if breaker != True:
-            print("YES")
+    numb = []
+    flag = 'YES'
+    for j in range(n):
+        numb.append(sys.stdin.readline().rstrip())
+    numb.sort()
+    for j in range(n-1):
+        if numb[j] == numb[j+1][:len(numb[j])]:
+            flag = 'NO'
+            break
+    print(flag)
