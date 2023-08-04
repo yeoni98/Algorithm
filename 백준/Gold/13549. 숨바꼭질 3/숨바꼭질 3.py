3fbf1cd59b76298f78acced28a646bd1cea75390
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-08-04 14:54:27 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 136 ms, Memory: 36180 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -2,24 +2,24 @@ import sys
 from collections import deque
 
 n, k = map(int,sys.stdin.readline().split())
-graph = [0]*100001
 visited = [False]*100001
+graph = [0]*100001
 q = deque()
 q.append(n)
 while q:
     x = q.popleft()
-    if x==k:
+
+    if x == k:
         print(graph[x])
         break
     for nx in (x-1,x+1,x*2):
-        if 0<=nx<=100000 and visited[nx] == False:
+        if 0<=nx<=100000 and visited[nx] == False and graph[nx] ==0:
+
             if nx == x*2:
                 graph[nx] = graph[x]
                 q.appendleft(nx)
+            #     순간이동 했을 때 소요되는 시간이 0초 이므로 우선순위 반영을 위해 appendleft를 쓴다.
             else:
-                graph[nx] = graph[x] + 1
+                graph[nx] = graph[x]+1
                 q.append(nx)
             visited[nx] = True
-
-
-
