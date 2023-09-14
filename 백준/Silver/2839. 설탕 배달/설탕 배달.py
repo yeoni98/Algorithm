658bdb24af7f4c605f48042b4597ce5911c23899
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-09-14 18:33:37 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,18 +1,18 @@
-n = int(input())
-l = []
+import sys
 
-if n%5 == 0:
-    l.append(n/5)
-elif n!=6 and n%5 == 1:
-    l.extend([2, (n-6)/5])
-elif n!=7 and n%5 == 2:
-    l.extend([4, (n-12)/5])
-elif n%5 == 3:
-    l.extend([1, (n-3)/5])
-elif n!=4 and n%5 == 4:
-    l.extend([3, (n-9)/5])
-elif n%3 == 0:
-    l.append(n/3)
+n = int(sys.stdin.readline())
+
+cnt3 = 0
+cnt5 = 0
+while n > 0:
+    if n % 5 == 0:
+        cnt5 += n // 5
+        n = 0
+        break
+    n -= 3
+    cnt3 += 1
+
+if n < 0:
+    print(-1)
 else:
-    l.append(-1)
-print(int(sum(l)))
+    print(cnt3 + cnt5)
