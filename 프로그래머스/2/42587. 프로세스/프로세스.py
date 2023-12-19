c9f601c61821e2139a8926f41a43afbfc4cc6e2f
최다연 <88011254+yeoni98@users.noreply.github.com>
2023-12-19 12:44:05 +0900

    [level 2] Title: 프로세스, Time: 0.31 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,31 @@
+
+from collections import deque
+
+def solution(priorities, location):
+    queue = deque((i,j) for i, j in enumerate(priorities))
+    ans = []
+    
+    while queue:
+        q = queue.popleft()
+        if queue and any(q[1]<qq[1] for qq in queue):
+            queue.append(q)
+        else:
+            ans.append(q)
+            
+        
+    for i in ans:
+        if i[0] == location:
+            return ans.index(i) + 1
+            
+            
+    
+#     m = max(priorities)
+#     counters = Counter(priorities)
+#     if counters[m] != 1:
+#         answer = location + 1
+#     elif counters[m] == 1:
+#         r = alphabet[location]
+#         start_index = priorities.index(max(priorities))
+#         ans = alphabet[start_index:]+alphabet[:start_index]
+#         answer = ans.index(r)+1
+    
